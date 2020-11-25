c068964d6face7b703bc6dd25335eed9f6f7e437
Amsel1676 <kuro.amsel@gmail.com>
2020-11-26 01:59:35 +0900

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from "@testing-library/react"
+import App from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('KbnButton', () => {
+    describe('プロパティ', () => {
+        describe('type', () => {
+            describe('デフォルト', () => {
+                test('button要素であること', () => {
+                    const { getByRole } = render(<App />)
+                    expect(getByRole('button')).toBeInTheDocument()
+                })
+            })
+        })
+    })
+})
