ff511e713272e3ddd935d7fe5c5b473ac7cad99c
Amsel1676 <kuro.amsel@gmail.com>
2020-12-01 23:02:59 +0900

    Manage form value by state

--- src/components/molecules/KbnLoginForm.tsx
+++ src/components/molecules/KbnLoginForm.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import KbnButton from '../atoms/KbnButton';
+import styles from './KbnLoginForm.module.css';
 
 function KbnLoginForm() {
+    const [emailForm, setEmailValue] = useState('');
+    const [passwordForm, setPasswordValue] = useState('');
+    const REGAX_EMAIL = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+    const handleChangeEmailForm = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmailValue(event.target.value)
+    }
+
+    const handleChangePasswordForm = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPasswordValue(event.target.value)
+    }
+
     return (
         <React.Fragment>
-            <label htmlFor="email">Email:</label>
-            <input id="email" type="text" />
-            <label htmlFor="password">Password:</label>
-            <input id="password" type="password" />
+            <div className={styles.loginForm}>
+                <label htmlFor="email">Email:</label>
+                <input id="email" type="text" value={emailForm} onChange={handleChangeEmailForm} />
+                <label htmlFor="password">Password:</label>
+                <input id="password" type="password" value={passwordForm} onChange={handleChangePasswordForm} />
+            </div>
             <KbnButton value="Login" />
         </React.Fragment>
     )
